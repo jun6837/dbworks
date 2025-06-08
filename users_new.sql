1ed29a3e459a91cd2ed58f319c51aca450387745
jun6837 <jun6837@naver.com>
2025-06-08 15:26:22 +0900

    bank

--- users_new.sql
+++ users_new.sql
@@ -1,8 +1,23 @@
 
 
-CREATE 
+CREATE TABLE users(
+    userid VARCHAR2(10) PRIMARY key,
+    userpassword VARCHAR2(100) NOT NULL,
+    username VARCHAR2(20) NOT NULL,
+    userage NUMBER(3)
+    
+);
+
+INSERT INTO users(userid, userpassword, username , userage) VALUES ('today', 't1357!','이정후',26);
+
+SELECT * FROM users;
+
+--이정후를 이종범으로 바꾸고 나이를 50세로 변경
+UPDATE users SET username = '이종범' , userage = 50 
+WHERE userid = 'today';
 
 
+commit;
 
 
 
